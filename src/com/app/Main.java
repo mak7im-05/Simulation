854563d7f27d0cdb1fddb383138cbbdff8867782
Maxim Karimov <karimovmaksim121@gmail.com>
2024-07-27 01:58:44 +0500

    mini fix

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -7,23 +7,3 @@ public class Main {
 
     }
 }
-
-//makeMove for all Creature // DONE
-//доделать astar algorigthm DONE
-//добавить проверки и почистить код DONE
-//починить генерацию в одной и той же клетке DONE
-//Generate random Grass Done
-//выделить функцию под генерацию координат DONE
-// IsGameOver() Done
-//WIDTH AND HEIGHT //DONE
-//Раскидать по папкам DONE
-//Node Вынести в другой файл // DONE
-// Механика ХП DONE
-//Счетчик Ходов DONE
-//Подумать над механикой скорости DONE
-//Поправить вывод в консоль DONE
-// Механика размножения DONE
-//Input basic setting for simulation DONE
-//Подобрать хорошие параметры для игры
-
-//Readme
